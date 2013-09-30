c1fb7c7a7016e29b0e18a1ec126eb14923437732
Robin Martinjak <rob@rmartinjak.de>
2013-09-30 23:30:06 +0200

    only use gzbuffer() if zlib recent enough

--- libecurve/include/ecurve/gz.h
+++ libecurve/include/ecurve/gz.h
@@ -16,11 +16,14 @@ typedef FILE *gzFile;
 #define gzread(stream, buf, n) ((int)fread(buf, 1, n, stream))
 #define gzwrite(stream, buf, n) ((int)fwrite(buf, 1, n, stream))
 #define gzgets(stream, s, size) fgets(s, size, stream)
-#define gzbuffer(file, size) 0
 #define gzopen fopen
 #define gzclose fclose
 #define gzprintf fprintf
 #define gzputc(stream, c) putc(c, stream)
+#endif
+
+#if ZLIB_VERNUM < 0x1235
+#define gzbuffer(file, size) 0
 #endif
 
 #endif
