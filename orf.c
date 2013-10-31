1c84274b626042ebedbe59b3e195d852a24bebb8
Robin Martinjak <rob@rmartinjak.de>
2013-10-31 10:01:07 +0100

    use new API in orf.c

--- orf.c
+++ orf.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+
 #include "ecurve.h"
 
 
@@ -10,6 +12,31 @@ enum opts {
     INFILE,
     ARGC,
 };
+
+bool
+orf_filter(const struct ec_orf *orf, const char *seq, size_t seq_len,
+        double seq_gc, void *opaque)
+{
+    size_t r, c, rows, cols;
+    struct ec_matrix *thresh = opaque;
+    (void) seq;
+    if (orf->length < 60) {
+        return false;
+    }
+    if (!thresh) {
+        return true;
+    }
+    ec_matrix_dimensions(thresh, &rows, &cols);
+    r = seq_gc * 100;
+    c = seq_len;
+    if (r >= rows) {
+        r = rows - 1;
+    }
+    if (c >= cols) {
+        c = cols - 1;
+    }
+    return orf->score >= ec_matrix_get(thresh, r, c);
+}
 
 int main(int argc, char **argv)
 {
@@ -23,14 +50,12 @@ int main(int argc, char **argv)
     struct ec_fasta_reader rd;
     ec_io_stream *stream;
 
-    char *buf = NULL;
-    size_t sz = 0;
-
-    enum ec_orf_mode mode = EC_ORF_ALL;
+    enum { MAX, ALL } mode;
+    void *filter_arg = NULL;
 
     if (argc < ARGC) {
         fprintf(stderr,
-                "usage: %s codon_scores_file (number|thresholds_file) [input_file]\n",
+                "usage: %s codon_scores_file (\"max\"|number|thresholds_file) [input_file]\n",
                 argv[0]);
         return EXIT_FAILURE;
     }
@@ -45,9 +70,10 @@ int main(int argc, char **argv)
     }
 
     if (!strcasecmp(argv[MIN_SCORE], "MAX")) {
-        mode = EC_ORF_MAX;
+        mode = MAX;
     }
     else {
+        mode = ALL;
         min_score = strtod(argv[MIN_SCORE], &endptr);
         if (*argv[MIN_SCORE] && *endptr) {
             res = ec_matrix_load_file(&orf_thresholds, argv[MIN_SCORE], EC_IO_GZIP);
@@ -62,6 +88,8 @@ int main(int argc, char **argv)
         else {
             ec_matrix_init(&orf_thresholds, 1, 1, &min_score);
         }
+
+        filter_arg = &orf_thresholds;
     }
 
     if (argc == ARGC && strcmp(argv[INFILE], "-")) {
@@ -77,17 +105,29 @@ int main(int argc, char **argv)
 
     ec_fasta_reader_init(&rd, 4096);
     while ((res = ec_fasta_read(stream, &rd)) == EC_ITER_YIELD) {
-        res = ec_orf_chained(rd.seq, mode, &codon_scores, &orf_thresholds, &buf, &sz);
-        if (EC_ISERROR(res)) {
-            break;
+        struct ec_orfiter oi;
+        struct ec_orf orf;
+
+        char *max_orf = NULL;
+        double max_score = -INFINITY;
+        ec_orfiter_init(&oi, rd.seq, &codon_scores, orf_filter, filter_arg);
+        while (ec_orfiter_next(&oi, &orf)) {
+            if (mode == MAX && orf.score > max_score) {
+                free(max_orf);
+                max_orf = strdup(orf.data);
+            }
+            else {
+                ec_fasta_write(ec_stdout, rd.header, rd.comment, orf.data, 0);
+            }
         }
-        if (buf && *buf) {
-            ec_fasta_write(ec_stdout, rd.header, rd.comment, buf, 0);
+        if (mode == MAX && max_orf) {
+            ec_fasta_write(ec_stdout, rd.header, rd.comment, max_orf, 0);
+            free(max_orf);
         }
+        ec_orfiter_destroy(&oi);
     }
     if (res != EC_ITER_STOP) {
         fprintf(stderr, "an error occured\n");
     }
-    free(buf);
     return EXIT_SUCCESS;
 }
