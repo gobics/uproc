64b86af4ccec2369ff64e50d32a56c8f42629a91
Robin Martinjak <rob@rmartinjak.de>
2013-11-21 22:21:51 +0100

    update doc comments in alphabet.h

--- libupro/include/upro/alphabet.h
+++ libupro/include/upro/alphabet.h
@@ -10,7 +10,8 @@
 #include <limits.h>
 
 /** Struct defining an amino acid alphabet */
-struct upro_alphabet {
+struct upro_alphabet
+{
     /** Original alphabet string */
     char str[UPRO_ALPHABET_SIZE + 1];
 
@@ -20,9 +21,14 @@ struct upro_alphabet {
 
 /** Initialize an alphabet object with the given string
  *
- * The string must be exactly `#UPRO_ALPHABET_SIZE` characters long and consist
+ * The string must be exactly ::UPRO_ALPHABET_SIZE characters long and consist
  * only of uppercase letters. No letter shall be included twice.
  *
+ * \return
+ * On success, returns 0.
+ * \return
+ * On failure, returns a negative value, sets ::upro_errno to ::UPRO_EINVAL and
+ * ::upro_errmsg accordingly.
  */
 int upro_alphabet_init(struct upro_alphabet *alpha, const char *s);
 
@@ -32,9 +38,13 @@ int upro_alphabet_init(struct upro_alphabet *alpha, const char *s);
  * \param c         character to translate
  *
  * \return
- * Corresponding amino acid or -1 if `c` is a non-alphabetic character.
+ * The value (between 0 and ::UPRO_ALPHABET_SIZE) of the corresponding amino
+ * acid or -1 if \c c is a non-alphabetic character.
+ *
+ * Does not set ::upro_errno.
  */
-upro_amino upro_alphabet_char_to_amino(const struct upro_alphabet *alpha, int c);
+upro_amino upro_alphabet_char_to_amino(const struct upro_alphabet *alpha,
+                                       int c);
 
 /** Translate amino acid to character
  *
@@ -43,7 +53,10 @@ upro_amino upro_alphabet_char_to_amino(const struct upro_alphabet *alpha, int c)
  *
  * \return
  * Corresponding character, or -1 if `a` does not represent a valid amino acid.
+ *
+ * Does not set ::upro_errno.
  */
-int upro_alphabet_amino_to_char(const struct upro_alphabet *alpha, upro_amino a);
+int upro_alphabet_amino_to_char(const struct upro_alphabet *alpha,
+                                upro_amino a);
 
 #endif
