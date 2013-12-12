dd3d995a6d7de3a69e53c282f35f22bc61a9657f
Robin Martinjak <rob@rmartinjak.de>
2013-12-12 12:08:39 +0100

    make import.c less repetitive

--- import.c
+++ import.c
@@ -56,97 +56,61 @@ print_usage(const char *progname)
         progname);
 }
 
-int
-export_ecurve(const char *dir, const char *name, uproc_io_stream *stream)
-{
-    int res;
-    struct uproc_ecurve ec;
-    fprintf(stderr, "exporting %s/%s.ecurve\n", dir, name);
-    res = uproc_storage_load(&ec, UPROC_STORAGE_BINARY, UPROC_IO_GZIP,
-                             "%s/%s.ecurve", dir, name);
-    if (res) {
-        return res;
-    }
-    res = uproc_storage_stores(&ec, UPROC_STORAGE_PLAIN, stream);
-    uproc_ecurve_destroy(&ec);
-    return res;
+#ifdef EXPORT
+#define EXPORT_FUNC(name, type, load, store, destroy)                       \
+int                                                                         \
+name(const char *dir, const char *name, uproc_io_stream *stream)            \
+{                                                                           \
+    int res;                                                                \
+    type thing;                                                             \
+    fprintf(stderr, "exporting %s/%s\n", dir, name);                        \
+    res = load(&thing, UPROC_IO_GZIP, "%s/%s", dir, name);                  \
+    if (res) {                                                              \
+        return res;                                                         \
+    }                                                                       \
+    res = store(&thing, stream);                                            \
+    destroy(&thing);                                                        \
+    return res;                                                             \
 }
 
-int
-import_ecurve(const char *dir, const char *name, uproc_io_stream *stream)
-{
-    int res;
-    struct uproc_ecurve ec;
-    fprintf(stderr, "importing %s/%s.ecurve (this will take several minutes)\n", dir, name);
-    res = uproc_storage_loads(&ec, UPROC_STORAGE_PLAIN, stream);
-    if (res) {
-        return res;
-    }
-    res = uproc_storage_store(&ec, UPROC_STORAGE_BINARY, UPROC_IO_GZIP,
-                              "%s/%s.ecurve", dir, name);
-    uproc_ecurve_destroy(&ec);
-    return res;
+#define ECURVE_LOAD(ptr, iotype, fmt, dir, name) \
+    uproc_storage_load(ptr, UPROC_STORAGE_BINARY, iotype, fmt, dir, name)
+#define ECURVE_STORES(ptr, stream) \
+    uproc_storage_stores(ptr, UPROC_STORAGE_PLAIN, stream)
+EXPORT_FUNC(ecurve, struct uproc_ecurve, ECURVE_LOAD, ECURVE_STORES,
+            uproc_ecurve_destroy)
+EXPORT_FUNC(matrix, struct uproc_matrix, uproc_matrix_load,
+            uproc_matrix_stores, uproc_matrix_destroy)
+EXPORT_FUNC(idmap, struct uproc_idmap, uproc_idmap_load,
+            uproc_idmap_stores, uproc_idmap_destroy)
+#else
+#define IMPORT_FUNC(name, type, load, store, destroy)                       \
+int                                                                         \
+name(const char *dir, const char *name, uproc_io_stream *stream)            \
+{                                                                           \
+    int res;                                                                \
+    type thing;                                                             \
+    fprintf(stderr, "importing %s/%s\n", dir, name);                        \
+    res = load(&thing, stream);                                             \
+    if (res) {                                                              \
+        return res;                                                         \
+    }                                                                       \
+    res = store(&thing, UPROC_IO_GZIP, "%s/%s", dir, name);                 \
+    destroy(&thing);                                                        \
+    return res;                                                             \
 }
 
-int
-export_matrix(const char *dir, const char *name, uproc_io_stream *stream)
-{
-    int res;
-    struct uproc_matrix m;
-    fprintf(stderr, "exporting %s/%s\n", dir, name);
-    res = uproc_matrix_load(&m, UPROC_IO_GZIP, "%s/%s", dir, name);
-    if (res) {
-        return res;
-    }
-    res = uproc_matrix_stores(&m, stream);
-    uproc_matrix_destroy(&m);
-    return res;
-}
-
-int
-import_matrix(const char *dir, const char *name, uproc_io_stream *stream)
-{
-    int res;
-    struct uproc_matrix m;
-    fprintf(stderr, "importing %s/%s\n", dir, name);
-    res = uproc_matrix_loads(&m, stream);
-    if (res) {
-        return res;
-    }
-    res = uproc_matrix_store(&m, UPROC_IO_GZIP, "%s/%s", dir, name);
-    uproc_matrix_destroy(&m);
-    return res;
-}
-
-int
-export_idmap(const char *dir, const char *name, uproc_io_stream *stream)
-{
-    int res;
-    struct uproc_idmap m;
-    fprintf(stderr, "exporting %s/%s\n", dir, name);
-    res = uproc_idmap_load(&m, UPROC_IO_GZIP, "%s/%s", dir, name);
-    if (res) {
-        return res;
-    }
-    res = uproc_idmap_stores(&m, stream);
-    uproc_idmap_destroy(&m);
-    return res;
-}
-
-int
-import_idmap(const char *dir, const char *name, uproc_io_stream *stream)
-{
-    int res;
-    struct uproc_idmap m;
-    fprintf(stderr, "importing %s/%s\n", dir, name);
-    res = uproc_idmap_loads(&m, stream);
-    if (res) {
-        return res;
-    }
-    res = uproc_idmap_store(&m, UPROC_IO_GZIP, "%s/%s", dir, name);
-    uproc_idmap_destroy(&m);
-    return res;
-}
+#define ECURVE_LOADS(ptr, stream) \
+    uproc_storage_loads(ptr, UPROC_STORAGE_PLAIN, stream)
+#define ECURVE_STORE(ptr, iotype, fmt, dir, name) \
+    uproc_storage_store(ptr, UPROC_STORAGE_BINARY, iotype, fmt, dir, name)
+IMPORT_FUNC(ecurve, struct uproc_ecurve, ECURVE_LOADS, ECURVE_STORE,
+            uproc_ecurve_destroy)
+IMPORT_FUNC(matrix, struct uproc_matrix, uproc_matrix_loads,
+            uproc_matrix_store, uproc_matrix_destroy)
+IMPORT_FUNC(idmap, struct uproc_idmap, uproc_idmap_loads,
+            uproc_idmap_store, uproc_idmap_destroy)
+#endif
 
 enum args
 {
@@ -159,6 +123,8 @@ int main(int argc, char **argv)
 {
     int res, opt;
     enum uproc_io_type iotype = UPROC_IO_GZIP;
+    uproc_io_stream *stream;
+    const char *dir, *file;
 
 #ifdef EXPORT
 #define SHORT_OPTS "hvn"
@@ -187,9 +153,11 @@ int main(int argc, char **argv)
             case 'v':
                 print_version();
                 return EXIT_SUCCESS;
+#ifdef EXPORT
             case 'n':
                 iotype = UPROC_IO_STDIO;
                 break;
+#endif
             case '?':
                 return EXIT_FAILURE;
         }
@@ -198,75 +166,46 @@ int main(int argc, char **argv)
         print_usage(argv[0]);
         return EXIT_FAILURE;
     }
-
-    uproc_io_stream *stream;
-    const char *src = argv[optind + SOURCE], *dest = argv[optind + DEST];
-
 #ifdef EXPORT
-    stream = uproc_io_open("w", iotype, "%s", dest);
-    if (!stream) {
-        uproc_perror("error opening %s", dest);
-        return EXIT_FAILURE;
-    }
-    res = export_matrix(src, "prot_thresh_e2", stream);
-    if (res) {
-        uproc_perror("error exporting matrix");
-        return EXIT_FAILURE;
-    }
-    res = export_matrix(src, "prot_thresh_e3", stream);
-    if (res) {
-        uproc_perror("error exporting matrix");
-        return EXIT_FAILURE;
-    }
-    res = export_idmap(src, "idmap", stream);
-    if (res) {
-        uproc_perror("error exporting idmap");
-        return EXIT_FAILURE;
-    }
-    res = export_ecurve(src, "fwd", stream);
-    if (res) {
-        uproc_perror("error exporting forward ecurve");
-        return EXIT_FAILURE;
-    }
-    res = export_ecurve(src, "rev", stream);
-    if (res) {
-        uproc_perror("error exporting reverse ecurve");
-        return EXIT_FAILURE;
-    }
-    uproc_io_close(stream);
-    return EXIT_SUCCESS;
+#define IMEX "ex"
+    dir = argv[optind + SOURCE];
+    file = argv[optind + DEST];
+    stream = uproc_io_open("w", iotype, "%s", file);
 #else
-    stream = uproc_io_open("r", UPROC_IO_GZIP, "%s", src);
+#define IMEX "im"
+    dir = argv[optind + DEST];
+    file = argv[optind + SOURCE];
+    stream = uproc_io_open("r", UPROC_IO_GZIP, "%s", file);
+#endif
     if (!stream) {
-        uproc_perror("error opening %s", src);
+        uproc_perror("error opening %s", file);
         return EXIT_FAILURE;
     }
-    res = import_matrix(dest, "prot_thresh_e2", stream);
+    res = matrix(dir, "prot_thresh_e2", stream);
     if (res) {
-        uproc_perror("error importing matrix");
+        uproc_perror("error " IMEX"porting matrix");
         return EXIT_FAILURE;
     }
-    res = import_matrix(dest, "prot_thresh_e3", stream);
+    res = matrix(dir, "prot_thresh_e3", stream);
     if (res) {
-        uproc_perror("error importing matrix");
+        uproc_perror("error " IMEX"porting matrix");
         return EXIT_FAILURE;
     }
-    res = import_idmap(dest, "idmap", stream);
+    res = idmap(dir, "idmap", stream);
     if (res) {
-        uproc_perror("error importing idmap");
+        uproc_perror("error " IMEX"porting idmap");
         return EXIT_FAILURE;
     }
-    res = import_ecurve(dest, "fwd", stream);
+    res = ecurve(dir, "fwd.ecurve", stream);
     if (res) {
-        uproc_perror("error importing forward ecurve");
+        uproc_perror("error " IMEX"porting forward ecurve");
         return EXIT_FAILURE;
     }
-    res = import_ecurve(dest, "rev", stream);
+    res = ecurve(dir, "rev.ecurve", stream);
     if (res) {
-        uproc_perror("error importing reverse ecurve");
+        uproc_perror("error " IMEX"porting reverse ecurve");
         return EXIT_FAILURE;
     }
     uproc_io_close(stream);
     return EXIT_SUCCESS;
-#endif
 }
