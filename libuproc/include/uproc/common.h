c2ed61df67b2b1ddafe4babdea2f74cc5492060a
Robin Martinjak <rob@rmartinjak.de>
2014-01-30 22:13:34 +0100

    add copyright note to common.h

--- libuproc/include/uproc/common.h
+++ libuproc/include/uproc/common.h
@@ -1,10 +1,28 @@
-#ifndef UPROC_COMMON_H
-#define UPROC_COMMON_H
+/* Copyright 2014 Peter Meinicke, Robin Martinjak
+ *
+ * This file is part of libuproc.
+ *
+ * libuproc is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * libuproc is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libuproc.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /** \file uproc/common.h
  *
  * Common definitions
  */
+
+#ifndef UPROC_COMMON_H
+#define UPROC_COMMON_H
 
 #include <stdint.h>
 #include <inttypes.h>
