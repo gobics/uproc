c910aba32560513a2f2a6dc9d9dfced231b4bfa3
Robin Martinjak <rob@rmartinjak.de>
2014-03-05 13:52:54 +0100

    update main page

--- libuproc/include/uproc.h
+++ libuproc/include/uproc.h
@@ -20,13 +20,21 @@
  *
  * \section sec_objects Types and objects
  *
- * \subsection subsec_opaque Opaque types
+ * If you browse the <a href="modules.html">Modules</a>, page, you will see
+ * quite a bunch of "modules" called "object uproc_..." or "struct uproc_...".
+ * These are the types defined by libuproc and they usually follow the
+ * conventions described below.
  *
- * Opaque types are generally defined as
+ * \subsection subsec_opaque Opaque object types
+ *
+ * Opaque object types are defined as forward declarations to structs hidden in
+ * the implementation (to ease API compatibility across different versions):
+ *
  * \code
- * typedef uproc_OBJECT_s uproc_OBJECT;
+ * typedef struct uproc_OBJECT_s uproc_OBJECT;
  * \endcode
  *
+ * \subsubsection subsec_opaque_create Creation
  * Objects of such type are created by calling the corresponding
  * \c uproc_OBJECT_create function, e.g.:
  *
@@ -34,6 +42,7 @@
  * uproc_bst *tree = uproc_bst_create(...);
  * \endcode
  *
+ * \subsubsection subsec_opaque_destroy Destruction
  * Every non-NULL pointer returned by \c uproc_OBJECT_create should be passed to
  * the corresponding \c uproc_OBJECT_destroy:
  *
@@ -41,15 +50,39 @@
  * uproc_bst_destroy(tree);
  * \endcode
  *
- * Passing NULL to any of the * \c uproc_OBJECT_destroy functions is a no-op
+ * Passing NULL to any of the \c uproc_OBJECT_destroy functions is a no-op
  * (and will NOT result in a nullpointer dereference).
+ *
+ *
+ * \subsubsection subsec_opaque_iter Iterator objects
+ *
+ * Certain objects are \e iterators (the ones whose OBJECT part ends in
+ * "iter").  These have a function \c uproc_OBJECT_next that returns 0 if an
+ * item was produced, 1 if the iterator is exhausted or -1 on error.
  *
  *
  * \subsection subsec_struct Struct types
  *
- * For a struct type \c struct \c uproc_OBJECT, if there is an initializer macro
- * called \c UPROC_OBJECT_INITIALIZER present, this initializer should be used
- * when defining objects of that type.
+ * \subsubsection subsec_struct_init Initialization
+ *
+ * For a struct type \c struct \c uproc_STRUCT, if there is an initializer
+ * macro called \c UPROC_STRUCT_INITIALIZER present, this initializer can be
+ * used when defining objects of that type.
+ * Alternatively, such a struct can be initialized by passing a pointer to it
+ * to the coresponding \c uproc_OBJECT_init function.
+ *
+ *
+ * \subsubsection subsec_struct_free Freeing struct members
+ *
+ * If the struct contains pointers to allocated storage, they can (and should)
+ * be freed using \c uproc_STRUCT_free. The values of affected members will be
+ * set to NULL.
+ *
+ *
+ * \subsubsection subsec_struct_copy Deep-copying
+ *
+ * If there is a corresponding \c uproc_STRUCT_copy function, it should be used
+ * to deep-copy the struct.
  *
  *
  * \section sec_multithread Multithreading and reentrancy
