b16efeef290a8f4bf9c73289385d2053d85949b3
Robin Martinjak <rob@rmartinjak.de>
2014-10-18 01:22:32 +0200

    exit if clang-format returns nonzero

--- format.bash
+++ format.bash
@@ -7,6 +7,6 @@ cd $repo_root
 
 for f in $(git ls-files '*.[ch]');
 do
-	clang-format -style file $f > indent_tmp_file;
+	clang-format -style file $f > indent_tmp_file || exit 1
 	mv indent_tmp_file $f
 done
